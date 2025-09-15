1671db37e732a7317e7a7180c825cfe836e8b813
drindr <jerrychancn@outlook.com>
2025-09-15 21:45:50 +0800

    add wayland-rs window instance

---
+++ rust-wayland-client/src/main.rs
@@ -0,0 +1,125 @@
+use wayland_client::{
+    delegate_noop,
+    protocol::{wl_buffer, wl_compositor, wl_registry, wl_surface},
+    Connection, Dispatch, QueueHandle,
+};
+use wayland_protocols::xdg::shell::client::{xdg_surface, xdg_toplevel, xdg_wm_base};
+#[derive(Default)]
+struct State {
+    pub running: bool,
+    pub base_surface: Option<wl_surface::WlSurface>,
+    pub xdg_surface: Option<(xdg_surface::XdgSurface, xdg_toplevel::XdgToplevel)>,
+    pub wm_base: Option<xdg_wm_base::XdgWmBase>,
+    pub configured: bool,
+    pub buffer: Option<wl_buffer::WlBuffer>,
+}
+impl Dispatch<wl_registry::WlRegistry, ()> for State {
+    fn event(
+        state: &mut Self,
+        registry: &wl_registry::WlRegistry,
+        event: wl_registry::Event,
+        _: &(),
+        _: &Connection,
+        qh: &QueueHandle<Self>,
+    ) {
+        if let wl_registry::Event::Global {
+            name, interface, ..
+        } = event
+        {
+            match interface.as_str() {
+                "wl_compositor" => {
+                    let compositor =
+                        registry.bind::<wl_compositor::WlCompositor, _, _>(name, 1, qh, ());
+                    let surface = compositor.create_surface(qh, ());
+                    state.base_surface = Some(surface);
+                    if state.wm_base.is_some() && state.xdg_surface.is_none() {
+                        state.init_xdg_state(qh);
+                    }
+                }
+                "xdg_wm_base" => {
+                    let wm_base = registry.bind::<xdg_wm_base::XdgWmBase, _, _>(name, 1, qh, ());
+                    state.wm_base = Some(wm_base);
+
+                    if state.base_surface.is_some() && state.xdg_surface.is_none() {
+                        state.init_xdg_state(qh);
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+}
+delegate_noop!(State: ignore wl_compositor::WlCompositor);
+delegate_noop!(State: ignore wl_surface::WlSurface);
+impl Dispatch<xdg_wm_base::XdgWmBase, ()> for State {
+    fn event(
+        _: &mut Self,
+        wm_base: &xdg_wm_base::XdgWmBase,
+        event: xdg_wm_base::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+    ) {
+        if let xdg_wm_base::Event::Ping { serial } = event {
+            wm_base.pong(serial);
+        }
+    }
+}
+impl Dispatch<xdg_toplevel::XdgToplevel, ()> for State {
+    fn event(
+        state: &mut Self,
+        _: &xdg_toplevel::XdgToplevel,
+        event: xdg_toplevel::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+    ) {
+        if let xdg_toplevel::Event::Close = event {
+            state.running = false;
+        }
+    }
+}
+impl Dispatch<xdg_surface::XdgSurface, ()> for State {
+    fn event(
+        state: &mut Self,
+        xdg_surface: &xdg_surface::XdgSurface,
+        event: xdg_surface::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+    ) {
+        if let xdg_surface::Event::Configure { serial, .. } = event {
+            xdg_surface.ack_configure(serial);
+            state.configured = true;
+            let surface = state.base_surface.as_ref().unwrap();
+            if let Some(ref buffer) = state.buffer {
+                surface.attach(Some(buffer), 0, 0);
+                surface.commit();
+            }
+        }
+    }
+}
+impl State {
+    pub fn init_xdg_state(&mut self, qh: &QueueHandle<Self>) {
+        let wm_base = self.wm_base.as_ref().unwrap();
+        let base_surface = self.base_surface.as_ref().unwrap();
+
+        let xdg_surface = wm_base.get_xdg_surface(base_surface, qh, ());
+        let top_level = xdg_surface.get_toplevel(qh, ());
+        top_level.set_title("Hello, Wayland!".to_owned());
+        base_surface.commit();
+        self.xdg_surface = Some((xdg_surface, top_level));
+    }
+}
+fn main() {
+    let conn = Connection::connect_to_env().unwrap();
+    let disp = conn.display();
+    let mut event_queue = conn.new_event_queue();
+    let qh = event_queue.handle();
+    let _registry = disp.get_registry(&qh, ());
+    let mut state = State::default();
+    state.running = true;
+    while state.running {
+        event_queue.blocking_dispatch(&mut state).unwrap();
+    }
+}
